b8ac16713990ac6b909c48b9aaa1487a0f511a1b
JayDub21 <justin.wofford.21@gmail.com>
2021-01-29 17:11:42 -0600

    fixed browserSync issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,37 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const browserSync = require("browser-sync").create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// npm command to manully pipe scss to css file: gulp style
 
 // compile scss into css
 function style() {
   // Find ANH scss file in scss folder
   return (
     gulp
-      .src("./public/main.scss")
+      .src('./public/main.scss')
 
       // Pass that file through sass compiler
-      .pipe(sass())
+      .pipe(sass().on('error', sass.logError))
 
       // Where compiled css to be saved
-      .pipe(gulp.dest("./public"))
+      .pipe(gulp.dest('./public'))
 
       // stream changes to all browser
       .pipe(browserSync.stream())
   );
 }
 
+// start watch function with command: gulp watch
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "./public"
-    }
+      baseDir: './public',
+    },
   });
-  gulp.watch("./public/main.scss", style);
-  gulp.watch("./public/*.html").on("change", browserSync.reload);
-  gulp.watch("./js/observers.js").on("change", browserSync.reload);
+  gulp.watch('./public/main.scss', style);
+  gulp.watch('./public/*.html').on('change', browserSync.reload);
+  gulp.watch('./js/observers.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
