ca1018d250c55a2fd3855d49bc36f325b938b2f9
Justin <justin.wofford.21@gmail.com>
2021-06-23 15:38:05 -0500

    dependabot alerts

--- mail.js
+++ mail.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
-const nodemailer = require("nodemailer");
-const mailGun = require("nodemailer-mailgun-transport");
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
   auth: {
     api_key: process.env.mailGun_key,
-    domain: "sandbox10ad0bc6d3ca4a19b953b5041cb87b72.mailgun.org"
-  }
+    domain: 'sandbox10ad0bc6d3ca4a19b953b5041cb87b72.mailgun.org',
+  },
 };
 
-const transporter = nodemailer.createTransport(mailGun(auth));
+let transporter = nodemailer.createTransport(mailGun(auth));
 
 const sendMail = (email, subject, text, cb) => {
   const mailOptions = {
     from: email,
-    to: "justin.wofford.21@gmail.com",
+    to: 'justin.wofford.21@gmail.com',
     subject,
-    text
+    text,
   };
 
-  transporter.sendMail(mailOptions, function(err, data) {
+  transporter.sendMail(mailOptions, function (err, data) {
     if (err) {
       cb(err, null);
     } else {
